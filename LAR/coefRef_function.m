7ed4e473e4728087f00d9c5c259d2836ca53d9d7
Anas BAHARI <anasbahari5@gmail.com>
2021-02-26 16:29:58 +0100

    calcul de LARs implémenté (y a une petite erreur de signe à régler après)

--- LAR/coefRef_function.m
+++ LAR/coefRef_function.m
@@ -1,17 +1,10 @@
-function [coef, thetap] = coefRef_function(signal, ordre)
-
-    % Calcul des parametres AR d'ordre P
-    r      = xcorr(signal, 'biased', ordre+1);
-    maxi_r = find(r == max(r));                 % L'indice du max de la correlation
-    rp0    = r(maxi_r:maxi_r+ordre-1);          % Les échantillons de 0 à P-1
-    Rp     = toeplitz(rp0);                     % La matrice de Toeplitz
-    invRp  = inv(Rp);                           % L'inverse de la matrice Toeplitz
-    rp1    = r(maxi_r+1:maxi_r+ordre);          % Les échantillons de 1 à P
-    rp1    = rp1';
-    thetap = zeros(1,ordre);                    % Les paramètres d'ordre P 
-    thetap(1,:) = (- invRp * rp1)';
-    % Calcul du coefficient de réflexion d'ordre P+1 : % a_{p+1}^{(p+1)}
-    rhop  = flip(rp1);
-    coef = (r(maxi_r+ordre+1) - sum(rhop.*thetap)) / (r(maxi_r) - sum(rp1.*thetap)); 
+function [coef, thetap_n] = coefRef_function(thetap_o, rp, indice)
+    
+    rp_new = rp(2:2+indice-1);
+    rhop  = flip(rp_new);
+    
+    coef = (rp(2+indice) - sum(rhop.*thetap_o)) / (rp(1) - sum(rp_new.*thetap_o)); 
+    betap_old = flip(thetap_o); 
+    thetap_n = [thetap_o - coef*betap_old, coef];
     
 end
