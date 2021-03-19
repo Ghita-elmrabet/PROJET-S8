e414f163c8e402d47f75162819f2be6a8f9b8239
Scialom Zachary <zscialom@enseirb-matmeca.fr>
2021-03-19 16:21:51 +0100

    Update LAR

--- src/LAR/lar_function.m
+++ src/LAR/lar_function.m
@@ -1,8 +1,11 @@
 function [LARs, coefs_ref, thetap_n] = lar_function(signal, max_ordre)
        
-    r      = xcorr(signal, 'biased', max_ordre);  % L'estimation biasée de la corrélation
+    r      = xcorr(signal, 'biased', max_ordre);  % L'estimation bias??e de la corr??lation
     maxi_r = find(r == max(r));                   % L'indice du max de la correlation
-    rp     = r(maxi_r:maxi_r+max_ordre);          % Les échantillons de 0 à P+1
+    rp     = r(maxi_r:maxi_r+max_ordre);          % Les ??chantillons de 0 ?? P+1
+    
+    rp_new = rp(2:2+max_ordre-2);
+    rhop_1  = flip(rp_new);
     
     coefs_ref = zeros(1,max_ordre);
     LARs      = zeros(1,max_ordre); 
@@ -12,9 +15,10 @@ function [LARs, coefs_ref, thetap_n] = lar_function(signal, max_ordre)
     thetap_o     = [coefs_ref(1)];
 
     for i=1:max_ordre-1       
-        [coefs_ref(i+1), thetap_n] = coefRef_function(thetap_o, rp, i);    
-        LARs(i+1) = log((1 + coefs_ref(i+1)) / (1 - coefs_ref(i+1)));        
-        thetap_o = thetap_n;       
+        [coefs_ref(i+1), thetap_n] = coefRef_function(thetap_o, rp, i); 
+        LARs(i+1) = -log((1 + coefs_ref(i+1)) / (1 - coefs_ref(i+1)));        
+        thetap_o = thetap_n;
+        %dernier_terme = rp(maxi_r+max_ordre)-sum(rhop_1.*thetap_o)/rp(maxi_r)+sum(rp_new.*thetap_o)
     end
-
+    
 end
--- tests/LAR/main_lar.m
+++ tests/LAR/main_lar.m
@@ -4,18 +4,18 @@ clear all
 close all
 clc;
 
-%% Paramètres
-Te = 1;            % Periode d'échantillonnage en s
-Fe = 1/Te;         % Fréquence d'échantillonnage en Hz
+%% Param??tres
+Te = 1;            % Periode d'??chantillonnage en s
+Fe = 1/Te;         % Fr??quence d'??chantillonnage en Hz
 N  = 1000;         % Nombre d echantillons du processus AR
-p  = 5;            % Ordre du Processus AR
+p  = 4;            % Ordre du Processus AR
 
 moy = 0; % Moyenne du BBGC
 sigma_caree = 1; % Variance du BBGC
 
 axe_temps = 0:Te:Te*N-Te;
 axe_freq  = -Fe/2:Fe/N:Fe/2-Fe/N;
-RSB = 10; % Rapport signal à bruit. à tester : -5dB, 0dB, 10dB
+RSB = 10; % Rapport signal ?? bruit. ?? tester : -5dB, 0dB, 10dB
 
 %% Generation d'un processus AR
 % Signal utile
@@ -25,11 +25,11 @@ RSB = 10; % Rapport signal à bruit. à tester : -5dB, 0dB, 10dB
 % modu_poles = [0.1,0.7,-0.8,0.9];      % -> bonne estimation
 modu_poles = [0.68,0.72,-0.75,0.7];     % -> bonne estimation
 
-par_ar    = poly(modu_poles);                    % Les paramètres AR
+par_ar    = poly(modu_poles);                    % Les param??tres AR
 
-bruit_ent = sigma_caree*randn(1,N);             % Le bruit BBGC en entrée du filtre
-num       = [1 0];                              % numérateur : zeros
-denom     = par_ar;                             % dénominateur : poles
+bruit_ent = sigma_caree*randn(1,N);             % Le bruit BBGC en entr??e du filtre
+num       = [1 0];                              % num??rateur : zeros
+denom     = par_ar;                             % d??nominateur : poles
 H         = freqz(num, denom, 2*pi*axe_freq);   % Focntion de transfert H(z)
 phi       = phasez(num, denom, 2*pi*axe_freq);
 proc_ar   = filter(num, denom, bruit_ent);      % Filtrage du BBGC avec H(z)
@@ -45,10 +45,10 @@ bruit_parasite = sigma_caree2*bruit;            % Bruit parasite
 y = proc_ar;
 % y = proc_ar + bruit_parasite; 
 
-%% Estimation des LARs en utilisant la réccurence de Levinson
+%% Estimation des LARs en utilisant la r??ccurence de Levinson
 [LAR, coefs_ref, thetap] = lar_function(y, p);
 
-%% Valider l'estimation en utilisant les fonctions pré-définies dans MATLAB
+%% Valider l'estimation en utilisant les fonctions pr??-d??finies dans MATLAB
 [ar_mat1, g]            = lpc(y, p);
 [ar_mat2, nvar, rc_mat] = aryule(y, p);
 LAR_mat                 = rc2lar(rc_mat);
@@ -56,37 +56,37 @@ LAR_mat                 = rc2lar(rc_mat);
 r(lg<0) = [];
 ar_mat3                 = levinson(r, p);
 
-%% Affichage des résultats de calculs
-fprintf("Les paramètres AR initiaux :\n");
+%% Affichage des r??sultats de calculs
+fprintf('Les parametres AR initiaux :\n');
 disp(par_ar);
-fprintf("Les paramètres AR estimés :\n");
+fprintf('Les param??tres AR estim??s :\n');
 disp([1,thetap]);
-fprintf("Les paramètres AR estimés par matlab (lpc):\n");
+fprintf('Les param??tres AR estim??s par matlab (lpc):\n');
 disp(ar_mat1);
-% fprintf("Les paramètres AR estimés par matlab (aryule):\n");
+% fprintf("Les param??tres AR estim??s par matlab (aryule):\n");
 % disp(ar_mat2);
-% fprintf("Les paramètres AR estimés par matlab (levinson):\n");
+% fprintf("Les param??tres AR estim??s par matlab (levinson):\n");
 % disp(ar_mat3);
-% fprintf("Les coefficients de réflexion :\n");
+% fprintf("Les coefficients de r??flexion :\n");
 % disp(coefs_ref);
-fprintf("Les Log-Area-Ratio :\n");
+fprintf('Les Log-Area-Ratio :\n');
 disp(LAR);
-fprintf("Les Log-Area-Ratio par matlab :\n");
+fprintf('Les Log-Area-Ratio par matlab :\n');
 disp(LAR_mat');
 
-%% Processus AR d'ordre p estimé : reconstruction du processus AR
-num_est   = [1 0];                                    % numérateur : zeros
-denom_est = [1,thetap];                               % dénominateur : poles
+%% Processus AR d'ordre p estim?? : reconstruction du processus AR
+num_est   = [1 0];                                    % num??rateur : zeros
+denom_est = [1,thetap];                               % d??nominateur : poles
 Hest     = freqz(num_est, denom_est, 2*pi*axe_freq); % Focntion de transfert H(z) estimee
 phi_est   = phasez(num_est, denom_est, 2*pi*axe_freq);
 
-nv_bruit_ent = sigma_caree*randn(1,N);             % Le bruit BBGC en entrée du filtre
+nv_bruit_ent = sigma_caree*randn(1,N);             % Le bruit BBGC en entr??e du filtre
 % proc_ar_est = filter(num_est, denom_est, bruit_ent);  % Filtrage d un BBGC avec H(z) estimee
 proc_ar_est = filter(num_est, denom_est, nv_bruit_ent);  % Filtrage d un BBGC avec H(z) estimee
 yest       = proc_ar_est;
 
 
-%% Aspect énergétique des PARs
+%% Aspect ??nerg??tique des PARs
 Yf = fftshift(abs(fft(y)));
 DSPy = sigma_caree*(abs(H).^2);
 Ps = (fftshift(abs(fft(y))).^2)/N;
@@ -95,25 +95,25 @@ Yfest = fftshift(abs(fft(yest)));
 DSPyest = sigma_caree*(abs(Hest).^2);
 Psest = (fftshift(abs(fft(yest))).^2)/N;
 
-%% Mesure de similarité entre les signaux 
-% Les périodogrammes des deux signaux
+%% Mesure de similarit?? entre les signaux 
+% Les p??riodogrammes des deux signaux
 [Py, contenu_freq]       = periodogram(y,[],[],Fe,'power');
 [Pyest, contenu_freqest] = periodogram(yest,[],[],Fe,'power');
 
 % Log-spectral distance
 DLS = sqrt(mean((10*log10(Ps./Psest)).^2));
 
-% L'intercorrélation entre les deux signaux 
+% L'intercorr??lation entre les deux signaux 
 Rxy = xcorr(y, yest);
 
 %% Affichage
 % figure,
 % plot(Rxy, 'r-')
 
-% Contenus fréquentiels des deux signaux
+% Contenus fr??quentiels des deux signaux
 figure,
-subplot(2,2,1), plot(axe_temps,y,'k'), title("y(t)"), xlabel('temps (s)'), grid on
-subplot(2,2,3), plot(axe_temps,yest), ylabel('y_{estimé}(t)'), xlabel('temps (s)'), grid on
+subplot(2,2,1), plot(axe_temps,y,'k'), title('y(t)'), xlabel('temps (s)'), grid on
+subplot(2,2,3), plot(axe_temps,yest), ylabel('y_{estim??}(t)'), xlabel('temps (s)'), grid on
 subplot(2,2,2), plot(contenu_freq,Py,'k'), title('Spectre de puissance'), ylabel('P1(f)'), grid on, axis tight
 subplot(2,2,4), plot(contenu_freqest,Pyest), xlabel('Frequence (Hz)'), ylabel('P2(f)'), grid on, axis tight
 
@@ -131,23 +131,23 @@ subplot(122), plot(axe_freq, DSPyest, 'r'), title('DSP du PAR'), xlabel('f_n')
 % plot(axe_temps, y) 
 % hold on
 % plot(axe_temps, y_est) 
-% xlabel('temps t'), ylabel('y_{estimé}(t)')
-% legend('y(t)', 'y_{estimé}(t)')
+% xlabel('temps t'), ylabel('y_{estim??}(t)')
+% legend('y(t)', 'y_{estim??}(t)')
 
 % figure,
 % subplot(131),
 % plot(axe_freq, 20*log10(abs(H))) 
 % hold on
 % plot(axe_freq, 20*log10(abs(H_est)))
-% legend('Le filtre initial', 'Le filtre estimé')
-% ylabel("L'amplitude en dB"), xlabel('fréquence normalisée')
+% legend('Le filtre initial', 'Le filtre estim??')
+% ylabel("L'amplitude en dB"), xlabel('fr??quence normalis??e')
 % grid
 % subplot(132),
 % plot(axe_freq, phi) 
 % hold on
 % plot(axe_freq, phi_est)
-% legend('Le filtre initial', 'Le filtre estimé')
-% ylabel("La phase en rad"), xlabel('fréquence normalisée')
+% legend('Le filtre initial', 'Le filtre estim??')
+% ylabel("La phase en rad"), xlabel('fr??quence normalis??e')
 % grid
 % subplot(133),
 % zplane(num, denom)
